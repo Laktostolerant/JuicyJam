430a1f5c1c4974a1692981ea7ca1487e8893a385
Tiger <tigominecraft@gmail.com>
2023-01-30 16:13:17 +0100

    sniper improvements

--- JuicyJam_Artware/Assets/SniperMovement.cs
+++ JuicyJam_Artware/Assets/SniperMovement.cs
@@ -1,42 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SniperMovement : MonoBehaviour
 {
-    enum EnemyState { RUNNING, GRAPPLED }
-    EnemyState currentState = EnemyState.GRAPPLED;
+    enum EnemyState { CHASING, GRAPPLING, NOAGGRO }
+    EnemyState currentState = EnemyState.CHASING;
 
+    GameObject player;
     CharacterController charCtrl;
+    NavMeshAgent navAgent;
     private Vector3 grapplePoint;
 
     RaycastHit hit;
 
     bool canGrappleAgain = true;
+    bool falling = false;
+    int aggroRange = 15;
 
     Coroutine repositionCoroutine;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
+        {
+            Debug.Log("No player found...");
+            GameObject.Destroy(gameObject);
+        }
         charCtrl = GetComponent<CharacterController>();
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
-    void Update()
+    private void Update()
     {
-        if (currentState == EnemyState.RUNNING)
-        {
+        if (currentState == EnemyState.CHASING)
             Chase();
-        }
-        else if (currentState == EnemyState.GRAPPLED)
-        {
+        else if (currentState == EnemyState.GRAPPLING)
             Grapple();
-        }
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("current state is " + currentState);
+        if(falling)
+            Gravity();
     }
 
     Vector3 NewPosition()
     {
         Debug.Log("YEEHAW");
-        currentState = EnemyState.GRAPPLED;
+        currentState = EnemyState.GRAPPLING;
         Vector3 newPos = transform.position;
         float distanceFromOrigin = 0;
         LayerMask grappleMask = 1 << 7 | 1 << 9;
@@ -67,11 +82,32 @@ public class SniperMovement : MonoBehaviour
 
     void Chase()
     {
+        if (currentState == EnemyState.NOAGGRO)
+            return;
 
+        Debug.Log("i be chasin");
+        currentState = EnemyState.CHASING;
+
+        if(Vector3.Distance(player.transform.position, transform.position) > aggroRange)
+        {
+            navAgent.destination = player.transform.position;
+        }
+        else
+        {
+            StartCoroutine(Reposition(Random.Range(4, 10)));
+        }
     }
     
     void Grapple()
     {
+        if (currentState == EnemyState.NOAGGRO)
+            return;
+
+        if(Vector3.Distance(player.transform.position, transform.position) > aggroRange && repositionCoroutine == null)
+        {
+            StartCoroutine(AggroCooldown());
+        }
+
         var moveOffset = grapplePoint - transform.position;
         charCtrl.Move(moveOffset * Time.fixedDeltaTime);
 
@@ -84,8 +120,41 @@ public class SniperMovement : MonoBehaviour
     IEnumerator Reposition(float delay)
     {
         canGrappleAgain = false;
-        grapplePoint = NewPosition();
+        if(currentState == EnemyState.GRAPPLING || currentState == EnemyState.CHASING)
+            grapplePoint = NewPosition();
         yield return new WaitForSeconds(delay);
         canGrappleAgain = true;
+    }
+
+    IEnumerator AggroCooldown()
+    {
+        currentState = EnemyState.NOAGGRO;
+        falling = true;
+        yield return new WaitUntil(() => !falling);
+        Chase();
+    }
+
+    void Gravity()
+    {
+        //charCtrl.Move(new Vector3(transform.position.x, -0.1f, transform.position.z) * Time.fixedDeltaTime * 0.00001f);
+        if (charCtrl.velocity.magnitude < 1)
+        {
+            Debug.Log("hi");
+            //falling = false;
+        }
+    }
+
+    Vector3 FloorPos()
+    {
+        Vector3 floor = transform.position;
+        LayerMask grappleMask = 1 << 7 | 1 << 9;
+        Vector3 characterCenter = transform.position + charCtrl.center;
+
+        if (Physics.Raycast(characterCenter, Vector3.down * 50, out hit, 30, grappleMask))
+        {
+            floor = hit.point;
+        }
+
+        return floor;
     }
 }
