9ada9b0d9344e8a04e98f9b9ab58aecf8739fd0a
Isabelle Höljer Heiskanen <102972428+Datseri@users.noreply.github.com>
2023-02-02 16:42:22 +0100

    shattered glas script done

--- JuicyJam_Artware/Assets/Bella/Scripts/ShatteredCaase.cs
+++ JuicyJam_Artware/Assets/Bella/Scripts/ShatteredCaase.cs
@@ -22,8 +22,13 @@ public class ShatteredCaase : MonoBehaviour, IDamageable
         if (HP <= 0)
         {
             for (int i = 0; i < rigidbodyAreas.Length; i++)
-            {
+            {                
+                rigidbodyAreas[i].GetComponent<Rigidbody>().AddExplosionForce(Random.Range(40f, 60f), 
+                    -transform.forward, Random.Range(20f, 40f), Random.Range(2f, 3f), ForceMode.Impulse);
+
+                rigidbodyAreas[i].GetComponent<Rigidbody>().drag = (Random.Range(10, 20));
                 rigidbodyAreas[i].GetComponent<Rigidbody>().useGravity = true;
+                rigidbodyAreas[i].GetComponent<BoxCollider>().enabled = true;
             }
             shatteredTheDiamondCase = true;
         }
