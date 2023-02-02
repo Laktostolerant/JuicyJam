9e09d453c793e48a55489bc5f401073e3bdc48eb
gittne <gustav.eriksson6@gmail.com>
2023-02-02 17:22:15 +0100

    Added ammo counter and changed testweapon to SMG

--- JuicyJam_Artware/Assets/GustavMapp/WeaponReloadBar.cs
+++ JuicyJam_Artware/Assets/GustavMapp/WeaponReloadBar.cs
@@ -1,26 +1,41 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class WeaponReloadBar : MonoBehaviour
 {
     GameObject Bar;
+    GameObject AmmoGameObject;
     [SerializeField] Image ProgressBar;
     [SerializeField] WeaponData weaponData;
+    [SerializeField] TextMeshProUGUI ammoCounter;
+    [SerializeField] TextMeshProUGUI ammoReserve;
+    int ammo;
+    int reserveAmmo;
     float remainingTime;
     float reloadTime;
 
     void Start()
     {
+        AmmoGameObject = gameObject.transform.Find("Ammo").gameObject;
         Bar = gameObject.transform.Find("Bar").gameObject;
         reloadTime = weaponData.reloadTime;
         remainingTime = reloadTime;
         Bar.SetActive(false);
+        reserveAmmo = weaponData.magSize;
     }
 
     void Update()
     {
+        ammo = weaponData.currentAmmo;
+
+        ammoCounter.text = ammo.ToString();
+
+        ammoReserve.text = reserveAmmo.ToString();
+
         if (remainingTime > 0 && weaponData.reloading)
         {
+            AmmoGameObject.SetActive(false);
             Bar.SetActive(true);
             remainingTime -= Time.deltaTime;
             ProgressBar.fillAmount = remainingTime / reloadTime;
@@ -28,6 +43,7 @@ public class WeaponReloadBar : MonoBehaviour
         }
         else
         {
+            AmmoGameObject.SetActive(true);
             Bar.SetActive(false);
             remainingTime = reloadTime;
         }
