c41b4986924180ae21b9f46d40a46a7f29105b1c
gittne <gustav.eriksson6@gmail.com>
2023-02-04 09:24:50 +0100

    implementerade ammocontroller script

--- JuicyJam_Artware/Assets/SlavaWorkSpace/Assets/Scripts/AmoController.cs
+++ JuicyJam_Artware/Assets/SlavaWorkSpace/Assets/Scripts/AmoController.cs
@@ -7,66 +7,37 @@ using UnityEngine.UI;
 public class AmoController : MonoBehaviour
 {
     [SerializeField]
-    private Text amoCounter;
+    private Text ammoCounter;
     [SerializeField]
-    private Slider amoBar;
-    private int currentAmo = 30;
-    private int fullAmo = 90;
+    private Slider ammoBar;
+    private int currentAmmo;
+    private int fullAmmo;
+    GameObject AmmoSlider;
+    [SerializeField] WeaponData weaponData;
 
-
-    
+    private void Start()
+    {
+        AmmoSlider = gameObject.transform.Find("Ammo").gameObject;
+        fullAmmo = weaponData.magSize;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        amoBar.value = fullAmo;
+        currentAmmo = weaponData.currentAmmo;
+        ammoBar.value = currentAmmo;
         if (Input.GetKeyDown(KeyCode.T) )
         {
-            fullAmo += 30;
+            fullAmmo += 30;
            
         }
        
-        if (Input.GetKeyDown(KeyCode.M) && currentAmo!=0)
+        if (Input.GetKeyDown(KeyCode.M) && currentAmmo!=0)
         {
-            currentAmo -= 10;
+            currentAmmo -= 10;
 
         }
-        amoCounter.text = currentAmo + "/" + fullAmo + " ";
-        
-
-
-        if (Input.GetKeyDown(KeyCode.R) || currentAmo == 0)
-        {
-            
-            Reload();
-            
-
-
-        }
-
+        ammoCounter.text = currentAmmo + "/" + fullAmmo + " ";
     }
-
-    public void Reload()
-    {
-        int reason = 30 - currentAmo;
-
-        if (fullAmo >= reason)
-        {
-            fullAmo = fullAmo - reason;
-            
-            currentAmo = 30;
-        }
-        else
-        {
-
-            currentAmo = currentAmo + fullAmo;
-            fullAmo = 0;
-        }
-      
-
-
-
-    }
-
     
 }
